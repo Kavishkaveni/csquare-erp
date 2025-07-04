9a3768c7bca32ffa76d00c58ef5673cee1c5e134
Kavishkaveni <mahendrankavishkaveni@gmail.com>
2025-07-04 22:35:39 +0530

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "assignment"; // make sure this matches the database name in phpMyAdmin
+$dbname = "assignment"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
